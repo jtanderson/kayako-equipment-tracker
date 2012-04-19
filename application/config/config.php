493c33ea2946fc31f278c75e5ae50d419074214f
Joe Anderson <jtanderson@ratiocaeli.com>
2012-04-19 00:34:42 -0400

    +Added a .htaccess file for easier rewrites and stuff

--- application/config/config.php
+++ application/config/config.php
@@ -30,7 +30,15 @@ $config['base_url']    = "$root";
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+
+if (function_exists('apache_get_modules')) {
+  $modules = apache_get_modules();
+  $mod_rewrite = in_array('mod_rewrite', $modules);
+} else {
+  $mod_rewrite =  getenv('HTTP_MOD_REWRITE')=='On' ? true : false ;
+}
+
+$config['index_page'] = $mod_rewrite ? "" : "index.php";
 
 /*
 |--------------------------------------------------------------------------
