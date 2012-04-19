4692fbd591e9de0c19f7d0870523fb7c3b4668a9
Joe Anderson <jtanderson@ratiocaeli.com>
2012-04-18 23:16:47 -0400

    *Beautification

--- install/index.php
+++ install/index.php
@@ -100,7 +100,7 @@ if($_POST) {
 					<fieldset id="install_panel_1">
 						<legend>Database</legend>
 						<p>This is the basic setup to connect to the database. The user should already exist, but the table will be created for you.</p>
-						<label for="hostname">Database Hostname</label><input type="text" id="hostname" value="localhost" class="input_text" name="hostname" />
+						<label for="hostname">Database Hostname</label><input type="text" id="hostname" value="localhost" class="input_text" placeholder="e.g. localhost" value="localhost" name="hostname" />
 						<label for="username">Database Username</label><input type="text" id="username" class="input_text" name="username" />
 						<label for="password">Database Password</label><input type="password" id="password" class="input_text" name="password" />
 						<label for="database">Database Name</label><input type="text" id="database" class="input_text" name="database" /><br/>
@@ -109,8 +109,8 @@ if($_POST) {
 					<fieldset id="install_panel_2" style="display:none;">
 						<legend>Kayako Fusion</legend>
 						<p>Enter the details for the system to access the Kayako Fusion server.  The URL is the base url of your Kayako server with no trailing slash.</p>
-		  		  		<label for="kayakoapi">Kayako API Key</label><input type="text" id="kayakoapi" class="input_text" name="kayakoapi" />
-				  		<label for="swifturl">Kayako URL</label><input type="text" id="swifturl" class="input_text" name="swifturl" />
+		  		  		<label for="kayakoapi">Kayako API Key</label><input type="text" id="kayakoapi" placeholder="e.g. 24c1a733-837f-57e4-f9e9-13de5c3cc671" class="input_text" name="kayakoapi" />
+				  		<label for="swifturl">Kayako URL (no trailing slash)</label><input type="text" placeholder="e.g. http://servicedesk.mydomain.com" id="swifturl" class="input_text" name="swifturl" />
 				  		<label for="kayakosecretkey">Kayako Secret Key</label><input type="text" id="kayakosecretkey" class="input_text" name="kayakosecretkey" /><br/>
 						<input type="button" class="btn" value="&#x2190; Back" onclick="previousPanel();" id="next_1" />
 						<input type="button" class="btn" value="Next &#x2192;" onclick="nextPanel();" id="next_1" />
